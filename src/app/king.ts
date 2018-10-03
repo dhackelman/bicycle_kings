08f821a6f1dca2c7123f1d5bad82a585c567d3a5
HACKELMAN <dh694n@att.com>
2018-10-03 12:53:16 -0400

    updating some styling

--- src/app/king.ts
+++ src/app/king.ts
@@ -1,4 +1,5 @@
 export class King {
     id: number; 
     name: string; 
+    image: string;
 }
--- src/app/mock-kings.ts
+++ src/app/mock-kings.ts
@@ -1,14 +1,63 @@
 import { King } from './king';
 
 export const KINGS: King[] = [
-  { id: 10, name: 'Chris Froome' },
-  { id: 9, name: 'Greg LeMond' },
-  { id: 8, name: 'Fausto Coppi' },
-  { id: 7, name: 'Alfons De Wolf' },
-  { id: 6, name: 'Marco Pantani' },
-  { id: 5, name: 'Major Taylor' },
-  { id: 4, name: 'Lance Armstrong' },
-  { id: 3, name: 'Bernard Hinault' },
-  { id: 2, name: 'Peter Sagan' },
-  { id: 1, name: 'Eddy Merckx' }
+  { 
+      id: 10, 
+      name: 'Chris Froome',
+      image: 'https://cdn.primedia.co.za/primedia-broadcasting/image/upload/c_fill,h_289,q_70,w_463/uzj5au3yhmmoc9algqw6', 
+  },
+  { 
+      id: 9, 
+      name: 'Greg LeMond',
+  image: '', 
+}
+      ,
+  { 
+      id: 8, 
+      name: 'Fausto Coppi',
+  image: '', 
+}
+      ,
+  { 
+      id: 7, 
+      name: 'Alfons De Wolf',
+      image: '', 
+      
+  },
+  { 
+      id: 6, 
+      name: 'Marco Pantani',
+  image: '', 
+}
+      ,
+  { 
+      id: 5, 
+      name: 'Major Taylor',
+  image: '', 
+}
+      ,
+  { 
+      id: 4, 
+      name: 'Lance Armstrong',
+  image: '', 
+}
+      ,
+  { 
+      id: 3, 
+      name: 'Bernard Hinault',
+  image: '', 
+}
+      ,
+  { 
+      id: 2, 
+      name: 'Peter Sagan',
+  image: '', 
+}
+      ,
+  { 
+      id: 1, 
+      name: 'Eddy Merckx',
+  image: '', 
+}
+      
 ];
